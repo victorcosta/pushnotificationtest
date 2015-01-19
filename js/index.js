bb68fe764703862a57888c9144b472e505a2be07
Victor Costa <victorcostaguitar@gmail.com>
2015-01-19 11:56:52 -0300

    removendo os codigo a mais

--- js/index.js
+++ js/index.js
@@ -34,8 +34,8 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        var pushNotification = window.plugins.pushNotification;
-        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"510667245122","ecb":"app.onNotificationGCM"});
+        // var pushNotification = window.plugins.pushNotification;
+        // pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"510667245122","ecb":"app.onNotificationGCM"});
 
     },
     // Update DOM on a Received Event
@@ -48,37 +48,38 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    },
-    successHandler: function(result) {
-        alert('Callback Success! Result = '+result)
-    },
-    errorHandler:function(error) {
-        alert(error);
-    },
-    onNotificationGCM: function(e) {
-        switch( e.event )
-        {
-            case 'registered':
-                if ( e.regid.length > 0 )
-                {
-                    console.log("Regid " + e.regid);
-                    alert('registration id = '+e.regid);
-                }
-            break;
-
-            case 'message':
-              // this is the actual push notification. its format depends on the data model from the push server
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-            break;
-
-            case 'error':
-              alert('GCM error = '+e.msg);
-            break;
-
-            default:
-              alert('An unknown GCM event has occurred');
-              break;
-        }
     }
+    // ,
+    // successHandler: function(result) {
+    //     alert('Callback Success! Result = '+result)
+    // },
+    // errorHandler:function(error) {
+    //     alert(error);
+    // },
+    // onNotificationGCM: function(e) {
+    //     switch( e.event )
+    //     {
+    //         case 'registered':
+    //             if ( e.regid.length > 0 )
+    //             {
+    //                 console.log("Regid " + e.regid);
+    //                 alert('registration id = '+e.regid);
+    //             }
+    //         break;
+
+    //         case 'message':
+    //           // this is the actual push notification. its format depends on the data model from the push server
+    //           alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+    //         break;
+
+    //         case 'error':
+    //           alert('GCM error = '+e.msg);
+    //         break;
+
+    //         default:
+    //           alert('An unknown GCM event has occurred');
+    //           break;
+    //     }
+    // }
 
 };
